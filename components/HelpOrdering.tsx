ceccb3c975729d5f5d4793bb4f23485fd0fb312c
Apple <apple@Apples-MacBook-Pro.local>
2026-07-08 16:48:22 +0530

    Add Help Ordering section and Build Your Own Care Box page

--- components/HelpOrdering.tsx
+++ components/HelpOrdering.tsx
@@ -57,7 +57,7 @@ export default function HelpOrdering() {
             <div className="text-5xl">❤️</div>
 
             <h3 className="text-2xl font-bold mt-5">
-              Order for Parents
+              Order for Elderly
             </h3>
 
             <p className="mt-4 text-gray-600">
